ff45eb16aefc40ed249055ada97943fef8e5b5bb
Tan Wang Leng <tanwangleng@outlook.com>
2018-12-28 12:20:20 +0800

    Site Update.

---
+++ markbind/layouts/default/scripts.js
@@ -0,0 +1,6 @@
+docsearch({ 
+    apiKey: '8310bb56d1ece8950bd20b0b8c080e10', 
+    indexName: 'se-edu', 
+    inputSelector: '#mysearch', 
+    debug: true // Set debug to true if you want to inspect the dropdown 
+    }); 
